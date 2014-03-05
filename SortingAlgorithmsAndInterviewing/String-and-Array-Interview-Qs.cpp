09575be4718153dc3e78f7f693dee2bd08e51032
elliotw44 <elliotw44@gmail.com>
2014-03-05 09:54:38 -0800

    moved tic tac toe to Specific-Qs.cpp

--- SortingAlgorithmsAndInterviewing/String-and-Array-Interview-Qs.cpp
+++ SortingAlgorithmsAndInterviewing/String-and-Array-Interview-Qs.cpp
@@ -347,33 +347,6 @@ vector<int> LongestIncreasingNumSet(vector<int> theArray){
 	return retArray;
 }
 
-string TicTacToeAnaly(vector< vectors<string>> board){
-     for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-           if(board[i][j] == ":x" || board[i][j] == ":o"){
-              if(i == 0 && j == 0){
-                  if(board[i][j] == board[i+1][j+1] && board[i][j] == board[i+2][j+2])
-                      return board[i][j]
-              }
-              if(i == 0){
-                  if(board[i][j] == board[i][j+1] && board[i][j] == board[i][j+2])
-                      return board[i][j]
-              }
-              if(j == 0){
-                  if(board[i][j] == board[i+1][j] && board[i][j] == board[i+2][j])
-                      return board[i][j]
-              }
-              if(j == 2 && i == 2){
-                  if(board[i][j] == board[i-1][j-1] && board[i][j] == board[i-2][j-2])
-                      return board[i][j]
-              }
-          }
-      }
-    }
-    return "nil"
-}
-
-
 //find all the a^2 + b^2 = c^2 combinations in a given array
 //O(n^2) Time
 void findTuples(vector<int> array){
